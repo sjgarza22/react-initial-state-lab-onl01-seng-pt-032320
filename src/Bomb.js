d83b38d331ee8716085ac0c0a0f6587f0aa89305
github username <github email address>
2020-12-13 22:18:59 -0600

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,28 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    countDown = () => {
+        let line = "";
+        const newCount = this.state.secondsLeft - 1;
+        if (this.state.secondsLeft > 0) {
+            this.setState({secondsLeft: newCount})
+            line = this.state.secondsLeft + " seconds left before I go boom!"
+        } else {
+            line = "Boom!"
+        }
+        return line;
+    }
+
+    render () {
+        return(
+        <p>{this.countDown()}</p>
+        )
+    }
+}
